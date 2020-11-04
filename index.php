613d63364373ec676f1e456510f93d502d1ab81a
ANDER414 <72255164+ANDER414@users.noreply.github.com>
2020-11-04 17:35:20 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,23 @@
 <?php
+/*
 $dbServername = "localhost";
-$dbUsername = "root";
+$dbUsername = "z51PQPB0f2";
 $dbPassword = "wIgBdKkfkz";
-$dbName = "z51PQPB0f2";
+$dbName = "z51PQPB0f2";*/
 
-$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
 
-echo"janel";
+
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'z51PQPB0f2');
+define('DB_PASSWORD', 'wmpCFCmz1h');
+define('DB_NAME', 'z51PQPB0f2');
+
+
+$conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+if($conn === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+
 $sql = "SELECT * FROM Masuratori;";
 $reult = mysqli_query($conn, $sql);
 $resultCheck = mysqli_num_rows($result);
