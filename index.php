f8fc4de0a756b5194f48f350dd575334a473c3ef
ANDER414 <72255164+ANDER414@users.noreply.github.com>
2020-11-04 16:41:01 +0200

    Update index.php

--- index.php
+++ index.php
@@ -7,5 +7,15 @@ $dbName = "z51PQPB0f2";
 $conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
 
 echo"janel";
+$sql = "SELECT * FROM Masuratori;";
+$reult = mysqli_query($conn, $sql);
+$resultCheck = mysqli_num_rows($result);
+
+if($resultCheck > 0) {
+while($row =mysqli_fetch_assoc($result))
+{
+  echo $row[]."<br>"
+}
+}
 
 ?>
